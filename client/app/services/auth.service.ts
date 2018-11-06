9f3e3950e3e0278d9cca7f63c97f775b60ad8d46
dwbowman1992 <dwbowman1992@gmail.com>
2018-11-05 18:37:04 -0600

    finalized heroku changes

--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+
+const APIEndpoint = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
     login(email: string, pwd: string) {
-      return this.http.post('http://localhost:3000/api/users/login', {
+      return this.http.post(APIEndpoint, {
         email: email,
         password: pwd
       });
--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://boyfriend-integration.herokuapp.com/api/users/login'
 };
